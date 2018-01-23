4f28d9ed60954330373b877f80f8125ce7fe967a
Linus Unnebäck <linus@folkdatorn.se>
2018-01-23 17:24:30 +0000

    🎉 Add Chrome support

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path')
 
+const unwrap = require('ts-unwrap')
 const utcVersion = require('utc-version')
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const GenerateJsonPlugin = require('generate-json-webpack-plugin')
+const RsvgWebpackPlugin = require('rsvg-webpack-plugin')
 
-const isProduction = (process.env.NODE_ENV === 'production')
+const isProduction = (unwrap(process.env.NODE_ENV) === 'production')
+const targetBrowser = unwrap(process.env.TARGET_BROWSER)
+
+const hasSvgSupport = (targetBrowser === 'firefox')
 
 const API_HOST = (isProduction ? 'https://api.ctrlpanel.io' : 'http://localhost:1834')
 const APP_HOST = (isProduction ? 'https://app.ctrlpanel.io' : 'http://localhost:1836')
@@ -18,7 +23,7 @@ module.exports = {
     popup: './source/popup.ts'
   },
   output: {
-    path: path.join(__dirname, 'distribution'),
+    path: path.join(__dirname, 'distribution', targetBrowser),
     filename: '[name].js'
   },
   node: {
@@ -83,20 +88,41 @@ module.exports = {
       'process.env.AUTO_SUBMIT': JSON.stringify(AUTO_SUBMIT),
       'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
     }),
+    (hasSvgSupport ? (
+      new CopyWebpackPlugin([
+        { from: `logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, context: 'assets', to: 'logo-dark.svg' },
+        { from: `logo-${isProduction ? targetBrowser : 'dev'}-light.svg`, context: 'assets', to: 'logo-light.svg' }
+      ])
+    ) : (
+      new RsvgWebpackPlugin([
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, name: 'logo-dark-16.png', width: 16, height: 16 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, name: 'logo-dark-32.png', width: 32, height: 32 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, name: 'logo-dark-48.png', width: 48, height: 48 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, name: 'logo-dark-64.png', width: 64, height: 64 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, name: 'logo-dark-96.png', width: 96, height: 96 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-dark.svg`, name: 'logo-dark-128.png', width: 128, height: 128 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-light.svg`, name: 'logo-light-16.png', width: 16, height: 16 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-light.svg`, name: 'logo-light-32.png', width: 32, height: 32 },
+        { file: `assets/logo-${isProduction ? targetBrowser : 'dev'}-light.svg`, name: 'logo-light-48.png', width: 48, height: 48 }
+      ])
+    )),
     new CopyWebpackPlugin([
-      { from: '*', context: 'assets' },
-      { from: '*', context: 'views' }
+      { from: 'popup.html', context: 'views' }
     ]),
     new GenerateJsonPlugin('manifest.json', {
       manifest_version: 2,
       name: (isProduction ? 'Ctrlpanel' : 'Ctrlpanel DEV'),
       version: utcVersion(),
 
-      applications: {
-        gecko: {
-          id: `{${EXT_ID}}`
-        }
+      icons: {
+        16: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-16.png'),
+        48: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-48.png'),
+        64: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-64.png'),
+        96: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-96.png'),
+        128: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-128.png')
       },
+
+      applications: (targetBrowser === 'firefox' ? { gecko: { id: `{${EXT_ID}}` } } : undefined),
 
       description: 'One-click sign in for accounts in Ctrlpanel.io',
       homepage_url: 'https://www.ctrlpanel.io/',
@@ -110,19 +136,16 @@ module.exports = {
       browser_action: {
         default_title: 'Ctrlpanel',
         default_popup: 'popup.html',
-        default_icon: `logo-${isProduction ? 'firefox' : 'dev'}-dark.svg`,
+        default_icon: {
+          16: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-16.png'),
+          32: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-32.png'),
+          48: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-48.png')
+        },
 
         theme_icons: [
-          {
-            dark: `logo-${isProduction ? 'firefox' : 'dev'}-dark.svg`,
-            light: `logo-${isProduction ? 'firefox' : 'dev'}-light.svg`,
-            size: 16
-          },
-          {
-            dark: `logo-${isProduction ? 'firefox' : 'dev'}-dark.svg`,
-            light: `logo-${isProduction ? 'firefox' : 'dev'}-light.svg`,
-            size: 32
-          }
+          { size: 16, dark: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-16.png'), light: (hasSvgSupport ? 'logo-light.svg' : 'logo-light-16.png') },
+          { size: 32, dark: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-32.png'), light: (hasSvgSupport ? 'logo-light.svg' : 'logo-light-32.png') },
+          { size: 48, dark: (hasSvgSupport ? 'logo-dark.svg' : 'logo-dark-48.png'), light: (hasSvgSupport ? 'logo-light.svg' : 'logo-light-48.png') }
         ]
       }
     })
