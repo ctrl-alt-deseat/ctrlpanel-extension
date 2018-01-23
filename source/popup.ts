7576beb5661dd4233b6bc6ca66b5a9c9089b9f78
Linus Unnebäck <linus@folkdatorn.se>
2018-01-23 16:05:32 +0000

    🌹 Use @wext for multi-platform compatibility

--- source/popup.ts
+++ source/popup.ts
@@ -1,6 +1,7 @@
 import unwrap = require('ts-unwrap')
 
 import CtrlpanelCore, { State } from '@ctrlpanel/core'
+import * as wextTabs from '@wext/tabs'
 
 import { API_HOST, APP_HOST, AUTO_SUBMIT } from './config'
 
@@ -18,26 +19,16 @@ const core = new CtrlpanelCore(API_HOST)
 
 let state: State = core.init()
 
-class CompleteListener {
-  private foobar = new Map<number, () => void>()
-
-  constructor () {
-    browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
-      if (changeInfo.status === 'complete') {
-        const resolver = this.foobar.get(tabId)
-
-        if (resolver) resolver()
+function waitForComplete (targetTabId: number) {
+  return new Promise((resolve, reject) => {
+    wextTabs.onUpdated.addListener(function listener (tabId, changeInfo) {
+      if (tabId === targetTabId && changeInfo.status === 'complete') {
+        wextTabs.onUpdated.removeListener(listener)
+        resolve()
       }
     })
-  }
-
-  listen (tabId: number) {
-    return new Promise((resolve) => this.foobar.set(tabId, resolve))
-  }
+  })
 }
-
-const completeListener = new CompleteListener()
-
 unlockForm.addEventListener('submit', async (ev) => {
   ev.preventDefault()
 
@@ -50,20 +41,20 @@ unlockForm.addEventListener('submit', async (ev) => {
   if (state.kind === 'empty') {
     statusMessage.textContent = 'Fetching credentials...'
 
-    const tab = unwrap(await browser.tabs.create({ active: false, url: `${APP_HOST}/#login` }))
+    const tab = unwrap(await wextTabs.create({ active: false, url: `${APP_HOST}/#login` }))
     const tabId = unwrap(tab.id)
 
-    await completeListener.listen(tabId)
+    await waitForComplete(tabId)
 
-    const syncToken = unwrap(await browser.tabs.executeScript(tabId, { code: 'window.localStorage.getItem("credentials")' }))[0] as string | undefined
+    const syncToken = unwrap(await wextTabs.executeScript(tabId, { code: 'window.localStorage.getItem("credentials")' }))[0] as string | undefined
 
     if (!syncToken) {
       statusMessage.textContent = 'Please log in to Ctrlpanel'
-      await browser.tabs.update(tabId, { active: true })
+      await wextTabs.update(tabId, { active: true })
       return
     }
 
-    await browser.tabs.remove(tabId)
+    await wextTabs.remove(tabId)
 
     state = core.init(syncToken)
   }
@@ -92,7 +83,7 @@ unlockForm.addEventListener('submit', async (ev) => {
   state = await core.sync(state)
 
   statusMessage.textContent = 'Findind account...'
-  const tab = (await browser.tabs.query({ active: true, currentWindow: true }))[0]
+  const tab = (await wextTabs.query({ active: true, currentWindow: true }))[0]
   const hostname = (new URL(unwrap(tab.url))).hostname.replace('www.', '')
 
   const data = core.getParsedEntries(state)
@@ -107,8 +98,8 @@ unlockForm.addEventListener('submit', async (ev) => {
   statusMessage.textContent = 'Filling...'
 
   try {
-    await browser.tabs.executeScript({ file: '/filler.js' })
-    await browser.tabs.executeScript({ code: `window.__ctrlpanel_extension_perform_login__(${JSON.stringify(facebook.handle)}, ${JSON.stringify(facebook.password)}, ${JSON.stringify(AUTO_SUBMIT)})` })
+    await wextTabs.executeScript({ file: '/filler.js' })
+    await wextTabs.executeScript({ code: `window.__ctrlpanel_extension_perform_login__(${JSON.stringify(facebook.handle)}, ${JSON.stringify(facebook.password)}, ${JSON.stringify(AUTO_SUBMIT)})` })
   } catch (_) {
     statusMessage.textContent = 'Failed to fill'
     return
